4762e22d35c1321734acef7d73b57c8d6dc08541
martynas.sebestinas <martynas@infotrans.lt>
2019-08-02 15:48:24 +0300

    repository .js delete axios.create

--- src/repositories/Repository.js
+++ src/repositories/Repository.js
@@ -1,9 +1,2 @@
 import axios from "axios";
-import { apiURL } from "../config/vue.config";
-
-const baseURL = apiURL;
-
-export default axios.create({
-    baseURL,
-    headers: {"Content-Type": "application/json"}
-});
+export default axios;
